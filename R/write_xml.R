e7436b08bd30ac6a8b20bab6cc646e4645d89a6a
Edwin de Jonge <edwindjonge@gmail.com>
2024-09-09 20:42:20 +0200

    adding write_xml for xmlbuilder

--- R/write_xml.R
+++ R/write_xml.R
@@ -12,3 +12,18 @@ write_xml.xml_fragment <- function(x, file, ...){
     list_as_xml_string() |>
     cat(file = file)
 }
+
+#' @exportS3Method xml2::write_xml
+write_xml.xmlbuilder <- function(x, file, ...){
+  if (!requireNamespace("xml2", quietly = TRUE)) {
+    stop("xml2 is required to use this function")
+  }
+
+  s <- x$to_xml_string()
+
+  if (length(s) != 1) {
+    stop("an xml_document must contain exactly one root element.", call. = FALSE)
+  }
+
+  cat(file = file)
+}
